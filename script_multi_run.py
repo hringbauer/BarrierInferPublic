640a0ae354896a4c1001f93dcc67011cff02d62f
Alex K <akolesnikov@ist.ac.at>
2017-03-27 13:25:20 +0200

    minor fixes

--- script_multi_run.py
+++ script_multi_run.py
@@ -5,16 +5,19 @@ Here you go Mr. AK - make it happen:-)
 
 from multi_run import fac_method
 
+import sys
+
+
 folder = "./nbh_folder/"   # Where the results are saved to.
 mp = 1                     # Whether to use MultiProcessing. 0: No 1: Yes
 
 MultiRun = fac_method("multi_nbh", folder, multi_processing = mp)  # Loads the right class.
 
 ########### For creating the data sets ###############
-'''Mr. Ak: Run the create_data_set thing again for 1 and 7. Probably delete the genotype and position file 
+'''Mr. Ak: Run the create_data_set thing again for 1 and 7. Probably delete the genotype and position file
 from the folder before so it saves cleanly.'''
 #data_set_nr = int(sys.argv[i])  # Which data-set to use
-data_set_nr = 30
+#data_set_nr = 30
 #MultiRun.create_data_set(data_set_nr)     # Creates data set and saves to Folder.
 
 
@@ -24,8 +27,8 @@ data_set_nr = 30
 ########### For analyzing the data sets###############
 '''Mr. AK: Run this for data-sets 10-12, 40-42, 60-62, and 80-82.
 One time for method=1 and one time for method=2'''
-i = int(sys.argv[i])           # Which data-set to use
-MultiRun.analyze_data_set(data_set_nr, method=1)   # Analyzes the results and pickles them.
+data_set_nr = int(sys.argv[1])           # Which data-set to use
+MultiRun.analyze_data_set(data_set_nr, method=2)   # Analyzes the results and pickles them.
 
 
 

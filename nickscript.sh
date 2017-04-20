56dc30273ec962001b30f12f4252e44e887a29d5
Harald Ringbauer <hringbauer@bjoern22.ista.local>
2017-04-20 12:35:03 +0200

    Data nbh_gauss Meth2 push

--- nickscript.sh
+++ nickscript.sh
@@ -3,12 +3,13 @@
 #$ -S /bin/bash
 #$ -v TST=abc
 #$ -M hringbauer@ist.ac.at
-#$ -N "BarrierSimulationsHarald"
+#$ -N "BarrierArrayHarald2"
 #$ -m ea
-#$ -l mf=50G
-#$ -l h_vmem=500G
-#$ -l h_rt=20:00:00
+#$ -l mf=10G
+#$ -l h_vmem=10G
+#$ -l h_rt=8:00:00
 #$ -cwd
+#$ -t 1-100:1
 
 export OMP_NUM_THREADS=1
-python script_bignick.py
+python script_bignick.py $SGE_TASK_ID
--- script_bignick.py
+++ script_bignick.py
@@ -7,7 +7,7 @@ from multi_run import fac_method
 import pickle as pickle
 import sys
 
-mp = 1  # Whether to use MultiProcessing. 0: No 1: Yes
+mp = 0  # Whether to use MultiProcessing. 0: No 1: Yes
 
         
 ###########Methods for creating Barrier Likelihood Profiles:
@@ -44,8 +44,9 @@ def analyze_barrier_strengths_ll():
     
 
 
-#data_set_nr = int(sys.argv[1])  # Which data-set to use
-data_set_nr = 1
+data_set_nr = int(sys.argv[1])  # Which data-set to use
+#data_set_nr = 1
+print("Starting Dataset Nr %i:" % data_set_nr)
 data_set_nr = data_set_nr - 1
 
 folder = "./nbh_folder_gauss/"  # Where the results are saved to.
@@ -55,12 +56,12 @@ MultiRun = fac_method("multi_nbh_gaussian", folder, multi_processing=mp)
 
 ########### For creating the data sets ###############
 
-MultiRun.create_data_set(data_set_nr)     # Creates data set and saves to Folder.
-MultiRun.analyze_data_set(data_set_nr, method=1)
-MultiRun.analyze_data_set(data_set_nr, method=2)  # Analyzes the results and pickles them.
-MultiRun.analyze_data_set(data_set_nr, method=0)
+#MultiRun.create_data_set(data_set_nr)     # Creates data set and saves to Folder.
+MultiRun.analyze_data_set(data_set_nr, method=2)
+#MultiRun.analyze_data_set(data_set_nr, method=0)  # Analyzes the results and pickles them.
+#MultiRun.analyze_data_set(data_set_nr, method=1)
 
-
+print("Run %i completed. Good job!" % data_set_nr)
 
 
 

11dfe1d05be8876d211b2971b56d650f2247bfcc
Harald RINGBAUER <hringbauer@ist.ac.at>
2017-05-22 15:19:17 +0200

    Serial Scripts

---
+++ serial_cluster.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#$ -S /bin/bash
+#$ -v TST=abc
+#$ -M hringbauer@ist.ac.at
+#$ -N "TestSerial"
+#$ -m ea
+#$ -l mf=48g
+#$ -l mem_free=48g
+#$ -l h_vmem=48g
+#$ -l h_rt=48:00:00
+#$ -cwd
+
+export OMP_NUM_THREADS=10
+module load tensorflow/python-2.7/1.1.0
+python serial_script.py
---
+++ serial_script.py
@@ -0,0 +1,58 @@
+'''
+Script to do serial Runs on the cluster.
+Loads everything and then does one run after another.
+'''
+
+from multi_run import fac_method
+import pickle as pickle
+import numpy as np
+import sys
+
+mp = 1 # Whether to use MultiProcessing. 0: No 1: Yes
+
+# data_set_nr = int(sys.argv[1])  # Which data-set to use
+
+# folder = "./nbh_folder_gauss1/"  # Where the results are saved to.
+# folder = "./cluster_folder/"
+# folder = "./bts_folder_test/"
+# folder = "./hz_folder/"
+folder = "./multi_ind_nr/"
+# folder ="./multi_loci_nr/"
+# folder = "./multi_2nd/"
+# folder = "./multi_2nd_b/"
+# folder = "./multi_barrier_synth/"
+
+# MultiRun = fac_method("multi_nbh", folder, multi_processing=mp)  # Loads the right class.
+# MultiRun = fac_method("multi_nbh_gaussian", folder, multi_processing=mp) 
+# MultiRun = fac_method("multi_cluster", folder, multi_processing=1)   
+# MultiRun = fac_method("multi_bts", folder, multi_processing=1)
+# MultiRun = fac_method("multi_HZ", folder, multi_processing=mp)
+MultiRun = fac_method("multi_inds", folder, multi_processing=mp)
+# MultiRun = fac_method("multi_2nd_cont", folder, multi_processing=mp)
+# MultiRun = fac_method("multi_barrier_pos", folder, multi_processing=mp)
+
+# MultiRun = fac_method("multi_loci", folder, multi_processing=mp)
+
+
+
+########### For creating and analyzing the data sets ###############
+#MultiRun.create_data_set(data_set_nr)     # Creates data set and saves to Folder.
+#MultiRun.create_data_set(data_set_nr, barrier_strength=0.05)
+
+
+########### For analyzing the data set ##############
+# MultiRun.analyze_data_set(data_set_nr, position_barrier=2.0, res_folder="barrier3/" ,method=2)  # Position Barrier is there for the HZ Data.
+# MultiRun.analyze_data_set(data_set_nr, position_barrier=-20.0, res_folder="barrier20m/" ,method=2)
+# MultiRun.analyze_data_set(data_set_nr, position_barrier=18.0, res_folder="barrier18m/" ,method=2)
+# MultiRun.analyze_data_set(data_set_nr, method=0)  # Analyzes the results and pickles them.
+# MultiRun.analyze_data_set(data_set_nr, method=1)
+
+for i in xrange(1,100):
+    print("Do Run Nr. %i" % i)
+    MultiRun.analyze_data_set(data_set_nr, method=0)
+    # MultiRun.analyze_data_set_cleaning(data_set_nr, method=2)
+
+print("Run %i completed. Good job!" % data_set_nr)
+
+
+

5b4a5ce1b9b0eed3ee69a18e9103df9f6629badc
Harald RINGBAUER <hringbauer@ist.ac.at>
2017-06-19 11:57:03 +0200

    Method 0 Update

--- serial_script.py
+++ serial_script.py
@@ -17,7 +17,8 @@ mp = 0  # Whether to use MultiProcessing. 0: No 1: Yes
 # folder = "./cluster_folder/"
 # folder = "./bts_folder_test/"
 # folder = "./hz_folder/"
-folder = "./multi_ind_nr/"
+# folder = "./multi_ind_nr/"
+folder = "./barrier_folder2/"
 # folder ="./multi_loci_nr/"
 # folder = "./multi_2nd/"
 # folder = "./multi_2nd_b/"
@@ -28,7 +29,8 @@ folder = "./multi_ind_nr/"
 # MultiRun = fac_method("multi_cluster", folder, multi_processing=1)   
 # MultiRun = fac_method("multi_bts", folder, multi_processing=1)
 # MultiRun = fac_method("multi_HZ", folder, multi_processing=mp)
-MultiRun = fac_method("multi_inds", folder, multi_processing=mp)
+#MultiRun = fac_method("multi_inds", folder, multi_processing=mp)
+MultiRun = fac_method("multi_barrier", folder, multi_processing=mp)
 # MultiRun = fac_method("multi_2nd_cont", folder, multi_processing=mp)
 # MultiRun = fac_method("multi_barrier_pos", folder, multi_processing=mp)
 
@@ -49,8 +51,8 @@ MultiRun = fac_method("multi_inds", folder, multi_processing=mp)
 # MultiRun.analyze_data_set(data_set_nr, method=1)
 
 #for i in xrange(1, 99):
-data_set_nr = int(sys.argv[1])  # Which data-set to use
-#data_set_nr = 99
+# data_set_nr = int(sys.argv[1])  # Which data-set to use
+data_set_nr = 50
 print("Do Run Nr. %i" % data_set_nr)
 MultiRun.analyze_data_set(data_set_nr, method=0)
 # MultiRun.analyze_data_set_cleaning(data_set_nr, method=2)

d90fb58703bbd76c3c57b59d74a34e96b71bc0db
Harald Ringbauer <hringbauer@bjoern22.ista.local>
2017-06-19 10:42:30 +0200

    MI Method1 Update

--- nickscript.sh
+++ nickscript.sh
@@ -3,15 +3,15 @@
 #$ -S /bin/bash
 #$ -v TST=abc
 #$ -M hringbauer@ist.ac.at
-#$ -N "AN_BTS"
+#$ -N "MULTI_IND1"
 #$ -m ea
 #$ -l mf=8G
 #$ -l mem_free=8G
 #$ -l h_vmem=8G
-#$ -l h_rt=18:00:00
+#$ -l h_rt=24:00:00
 #$ -cwd
-#$ -t 200-500:25
+#$ -t 1-100:1
 
 export OMP_NUM_THREADS=1
-#python script_bignick.py $SGE_TASK_ID
-echo $SGE_TASK_ID
+python script_bignick.py $SGE_TASK_ID
+# echo $SGE_TASK_ID

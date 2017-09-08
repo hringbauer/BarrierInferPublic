f86346fc778aaf60e89b7243034cc5c7ab834024
Harald Ringbauer <hringbauer@bjoern22.ista.local>
2017-09-08 10:27:49 +0200

    Results 14 and rerun all

--- nickscript.sh
+++ nickscript.sh
@@ -3,14 +3,14 @@
 #$ -S /bin/bash
 #$ -v TST=abc
 #$ -M hringbauer@ist.ac.at
-#$ -N "HZALL.v2.0"
+#$ -N "HZALL.v2.2"
 #$ -m a    
 #$ -l mf=4G
 #$ -l mem_free=4G
 #$ -l h_vmem=4G
 #$ -l h_rt=72:00:00
 #$ -cwd
-#$ -t 1-250:1
+#$ -t 1-230:1
 
 export OMP_NUM_THREADS=1
 python script_bignick.py $SGE_TASK_ID

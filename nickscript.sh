3fad9151307307d9492ba078bb5b5e5b76177791
Harald Ringbauer <hringbauer@bjoern22.ista.local>
2017-10-12 13:51:59 +0200

    Result HZSynth

--- nickscript.sh
+++ nickscript.sh
@@ -3,14 +3,14 @@
 #$ -S /bin/bash
 #$ -v TST=abc
 #$ -M hringbauer@ist.ac.at
-#$ -N "HZALL.v2.5"
+#$ -N "HZSynthv0"
 #$ -m a    
 #$ -l mf=4G
 #$ -l mem_free=4G
 #$ -l h_vmem=4G
 #$ -l h_rt=72:00:00
 #$ -cwd
-#$ -t 250-500:1
+#$ -t 1-300:1
 
 export OMP_NUM_THREADS=1
 python script_bignick.py $SGE_TASK_ID
